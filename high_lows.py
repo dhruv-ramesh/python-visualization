3147064aea349451c43e29772ef935997b56d643
Dhruv Ramesh <dhruvramesh1@gmail.com>
2023-03-17 08:35:30 -0400

    Plotting sitka weather data complete

--- high_lows.py
+++ high_lows.py
@@ -1,23 +1,31 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-filename = 'weather_data/sitka_weather_07-2021_simple.csv'
+filename = 'weather_data/sitka_weather_2021_simple.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     highs = []
+    lows = []
+    dates = []
     for row in reader:
         highs.append(int(row[4]))
+        lows.append(int(row[-1]))
+        dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
 
 # Plot data
 fig = plt.figure(dpi = 128, figsize =(10,6))
-plt.plot(highs, c = 'red')
+plt.plot(dates, highs, c = 'red', alpha = 0.5)
+plt.plot(dates, lows, c = 'blue', alpha = 0.5)
+plt.fill_between(dates, highs, lows, facecolor = 'blue', alpha = 0.1)
 
 # Format the plot
-plt.title('Daily high temperature, July 2021', fontsize = 16)
+plt.title('Daily high and low temperatures-2021', fontsize = 16)
 plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize = 16)
 plt.tick_params(axis = 'both', which = 'major', labelsize = 16)
 
